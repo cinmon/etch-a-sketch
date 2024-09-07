7bed5587e85151418625b758caabb55369b81b55
cinmon <gianellaalessandrac@gmail.com>
2024-09-07 16:58:36 +0900

    Change hover logic to js instead of css

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ for(let i = 1 ; i <= (16 * 16) ; i++) {
     }
 
 }
+
+const squares_list = document.querySelectorAll(".square")
+
+squares_list.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.transition = "background-color ease";
+        console.log("Mouse over square.")
+        square.style.backgroundColor = "#F6D6D6";
+        square.style.transform = "scale(1.2)";
+    });
+    square.addEventListener("mouseout", ()=> {
+        square.style.transition = "background-color 0.5s ease";
+        square.style.backgroundColor = "#353535";
+        square.style.transform = "scale(1)";
+    });
+});
